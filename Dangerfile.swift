cfacebe6b10b74902a5f2cec840fd6293a1f7b62
Orta Therox <orta.therox@gmail.com>
2017-08-25 07:28:03 -0400

    Handle taking some of the JSON and presenting it as a DSL

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,7 +1,13 @@
 import Danger
 
-
 print("Evaluating Dangerfile")
+
+let danger = Danger()
+
+print("Modified:")
+for file in danger.git.modifiedFiles {
+    print(" - " + file)
+}
 
 warn("Warning: bad stuff")
 fail("Failure: bad stuff happened")
