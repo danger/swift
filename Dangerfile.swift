47c8729c65e03de5854f7b17f9a6179619b7dd86
Orta Therox <orta.therox@gmail.com>
2017-09-04 08:50:02 -0400

    Fix the dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -7,7 +7,7 @@ let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
 let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
 let sourceChanges = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
 
-if !changelogChanged && sourceChanges {
+if !changelogChanged && sourceChanges != nil {
     warn("No CHANGELOG entry added.")
 }
 
