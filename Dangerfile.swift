57741ada6b9dbd203a8208fd347f894875ef180c
Orta Therox <orta.therox@gmail.com>
2017-11-25 10:04:28 -0500

    Updates the Dangerfile

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Danger
 
 let danger = Danger()
@@ -6,7 +7,20 @@ let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
 let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
 let sourceChanges = allSourceFiles.first(where: { $0.hasPrefix("Sources") })
+let isTrivial = danger.github.pullRequest.title.contains("#trivial")
 
-if !changelogChanged && sourceChanges != nil {
-    warn("No CHANGELOG entry added.")
+if (danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 10) {
+    warn("Big PR, try to keep changes smaller if you can")
+}
+
+if !isTrivial && !changelogChanged && sourceChanges != nil {
+    warn("""
+     Any changes to library code should be reflected in the Changelog.
+
+     Please consider adding a note there and adhere to the [Changelog Guidelines](https://github.com/Moya/contributors/blob/master/Changelog%20Guidelines.md).
+    """)
+}
+
+if danger.github.pullRequest.title.contains("WIP") {
+    warn("PR is classed as Work in Progress")
 }
