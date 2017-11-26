7913193052177b9192660a05b2ca945a675e670c
Orta Therox <orta.therox@gmail.com>
2017-11-26 07:38:07 -0500

    Exit with -1 on an exception

--- Sources/Runner/main.swift
+++ Sources/Runner/main.swift
@@ -1,9 +1,12 @@
 import Foundation
 
 let cliLength = ProcessInfo.processInfo.arguments.count
-
-if cliLength > 1 && "edit" == CommandLine.arguments[1] {
-    try editDanger()
-} else {
-    try runDanger()
+do {
+    if cliLength > 1 && "edit" == CommandLine.arguments[1] {
+        try editDanger()
+    } else {
+        try runDanger()
+    }
+} catch {
+    exit(1)
 }
