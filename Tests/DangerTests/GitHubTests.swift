ab7ea3b3ddd0a125883cacaf22c36884a41ac88e
Joshua Kaplan <yhkaplan@gmail.com>
2018-03-24 14:07:41 +0900

    Added test for GitHubUser

--- Tests/DangerTests/GitHubTests.swift
+++ Tests/DangerTests/GitHubTests.swift
@@ -1,14 +1,21 @@
 import XCTest
-import Danger
+@testable import Danger
 
 class GitHubTests: XCTestCase {
     static var allTests = [
-        ("testExample", testExample),
+        ("test_GitHubUser_decode", test_GitHubUser_decode),
     ]
-
-    func testExample() {
-        
-    }
     
+    func test_GitHubUser_decode() throws {
+        guard let data = GitHubUserJSON.data(using: .utf8) else {
+            XCTFail("Cannot generate Data")
+            return
+        }
+        
+        let user: GitHubUser = try JSONDecoder().decode(GitHubUser.self, from: data)
+        
+        XCTAssertEqual("yhkaplan", user.login)
+        XCTAssertEqual(25879490, user.id)
+        XCTAssertEqual(GitHubUser.UserType.user, user.userType)
+    }
 }
-
