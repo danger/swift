f8ceb5b0b6b0542d44fe03570e2b5aa116922385
Joshua Kaplan <yhkaplan@gmail.com>
2018-03-24 15:23:22 +0900

    Added test for GitHubMilestone

--- Tests/DangerTests/GitHubTests.swift
+++ Tests/DangerTests/GitHubTests.swift
@@ -4,18 +4,69 @@ import XCTest
 class GitHubTests: XCTestCase {
     static var allTests = [
         ("test_GitHubUser_decode", test_GitHubUser_decode),
+        ("test_GitHubMilestone_decodeWithSomeParameters", test_GitHubMilestone_decodeWithSomeParameters),
+        ("test_GitHubMilestone_decodeWithAllParameters", test_GitHubMilestone_decodeWithAllParameters),
     ]
     
     func test_GitHubUser_decode() throws {
         guard let data = GitHubUserJSON.data(using: .utf8) else {
-            XCTFail("Cannot generate Data")
+            XCTFail("Cannot generate data")
             return
         }
         
-        let user: GitHubUser = try JSONDecoder().decode(GitHubUser.self, from: data)
+        let correctUser = GitHubUser(id: 25879490, login: "yhkaplan", userType: .user)
+        let testUser: GitHubUser = try JSONDecoder().decode(GitHubUser.self, from: data)
         
-        XCTAssertEqual("yhkaplan", user.login)
-        XCTAssertEqual(25879490, user.id)
-        XCTAssertEqual(GitHubUser.UserType.user, user.userType)
+        XCTAssertEqual(testUser, correctUser)
+    }
+    
+    func test_GitHubMilestone_decodeWithSomeParameters() throws {
+        // This is only a temporary hack
+        // TODO: move Danger.swift's date formatter to Date extension
+        // with its own test
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZ"
+        
+        guard let data = GitHubMilestoneJSONWithSomeParameters.data(using: .utf8),
+        let createdAt = dateFormatter.date(from: "2018-01-20T16:29:28Z"),
+            let updatedAt = dateFormatter.date(from: "2018-02-27T06:23:58Z") else {
+            XCTFail("Cannot generate data")
+            return
+        }
+        
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(dateFormatter)
+        
+        let milestone: GitHubMilestone = try decoder.decode(GitHubMilestone.self, from: data)
+        let creator = GitHubUser(id: 739696, login: "rnystrom", userType: .user)
+        
+        XCTAssertNil(milestone.closedAt)
+        XCTAssertEqual(milestone.closedIssues, 2)
+        XCTAssertEqual(milestone.createdAt, createdAt)
+        XCTAssertEqual(milestone.creator, creator)
+        XCTAssertEqual(milestone.description, "kdsjfls")
+        XCTAssertNil(milestone.dueOn)
+        XCTAssertEqual(milestone.id, 3050458)
+        XCTAssertEqual(milestone.number, 11)
+        XCTAssertEqual(milestone.openIssues, 0)
+        XCTAssertEqual(milestone.state, GitHubMilestone.MilestoneState.open)
+        XCTAssertEqual(milestone.title, "1.19.0")
+        XCTAssertEqual(milestone.updatedAt, updatedAt)
+    }
+    
+    func test_GitHubMilestone_decodeWithAllParameters() throws {
+        
+    }
+}
+
+// TODO: Delete this implementation when Swift 4.1 comes out
+extension GitHubUser: Equatable {
+    public static func ==(lhs: GitHubUser, rhs: GitHubUser) -> Bool {
+        return
+            lhs.id == rhs.id &&
+            lhs.userType == rhs.userType &&
+            lhs.login == rhs.login
     }
 }
