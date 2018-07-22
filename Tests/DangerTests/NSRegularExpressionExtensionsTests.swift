3d42fc889b84992ea2f6dc237f0b3b8807a5000c
Joshua Kaplan <yhkaplan@gmail.com>
2018-07-22 09:57:38 +0900

    Add tests for firstMatchingString(in string:)

---
+++ Tests/DangerTests/NSRegularExpressionExtensionsTests.swift
@@ -0,0 +1,39 @@
+import XCTest
+@testable import Danger
+
+class NSRegularExpressionExtensionsTests: XCTestCase {
+    
+    static var allTests = [
+        ("test_firstMatchingString_passingRegex", test_firstMatchingString_passingRegex),
+        ("test_firstMatchingString_failingRegex", test_firstMatchingString_failingRegex)
+    ]
+    
+    let string = "Dogs and cats were wearing hats"
+    
+    func test_firstMatchingString_passingRegex() {
+        let pattern = "(cats|hats)$"
+        let expectedMatch = "hats"
+        
+        guard
+            let expression = try? NSRegularExpression(pattern: pattern),
+            let testMatch = expression.firstMatchingString(in: string)
+        else {
+            XCTFail(); return
+        }
+        
+        XCTAssertEqual(testMatch, expectedMatch)
+    }
+    
+    func test_firstMatchingString_failingRegex() {
+        let pattern = "^(cats|hats)"
+        
+        guard let expression = try? NSRegularExpression(pattern: pattern) else {
+                XCTFail(); return
+        }
+        
+        let testMatch = expression.firstMatchingString(in: string)
+        
+        XCTAssertNil(testMatch)
+    }
+    
+}
