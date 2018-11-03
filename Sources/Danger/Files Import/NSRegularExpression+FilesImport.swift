16d462a981796036d665def8e30856dc20cb8a64
Franco Meloni <franco.meloni91@gmail.com>
2018-11-03 11:13:54 +0000

    Added DangerFileGenerator class to create a Dangerfile that contains all the imported files

---
+++ Sources/Danger/Files Import/NSRegularExpression+FilesImport.swift
@@ -0,0 +1,14 @@
+//
+//  NSRegularExpression+FilesImport.swift
+//  Danger
+//
+//  Created by Franco Meloni on 01/11/2018.
+//
+
+import Foundation
+
+extension NSRegularExpression {
+    private static let fileImportPattern = "\\/\\/[\\ ]?fileImport:\\ (.*)"
+    
+    static let filesImportRegex = try! NSRegularExpression(pattern: fileImportPattern, options: .allowCommentsAndWhitespace)
+}
