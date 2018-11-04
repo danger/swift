f6f027d94379714f834119bcedae7fc9887ecf8b
Franco Meloni <franco.meloni91@gmail.com>
2018-11-04 11:55:40 +0000

    Put changelogCheck inside a function

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -6,6 +6,7 @@ import Danger
 let danger = Danger()
 
 // fileImport: DangerfileExtensions/ChangelogCheck.swift
+checkChangelog()
 
 if (danger.git.createdFiles.count + danger.git.modifiedFiles.count - danger.git.deletedFiles.count > 10) {
     warn("Big PR, try to keep changes smaller if you can")
