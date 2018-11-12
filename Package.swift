1b9034c5517ac0e90726a8520c9589135f5ede24
Franco Meloni <franco.meloni91@gmail.com>
2018-11-12 16:53:48 +0000

    Updated octokit to version 0.9.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/JohnSundell/Marathon.git", from: "3.1.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.1.0"),
-        .package(url: "https://github.com/f-meloni/octokit.swift", .branch("work_with_linux"))
+        .package(url: "https://github.com/nerdishbynature/octokit.swift", from: "0.9.0")
     ],
     targets: [
         .target(name: "Danger", dependencies: ["ShellOut", "OctoKit"]),
