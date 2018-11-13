5bc06a719fc7b6e7abccb226a4a7c35943f1600a
Franco Meloni <franco.meloni91@gmail.com>
2018-11-13 07:57:29 +0000

    Remove value(forKeyPath:) method call because is not supported by Linux

--- Sources/Danger/CliArgsParser.swift
+++ Sources/Danger/CliArgsParser.swift
@@ -11,8 +11,10 @@ public final class CliArgsParser {
     public init() {}
     
     public func parseCli(fromData data: Data) -> CliArgs? {
-        if let dictionary = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary,
-            let cliArgsDictionary = dictionary?.value(forKeyPath: "danger.settings.cliArgs") as? [String:Any] {
+        if let dictionary = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String:Any],
+            let danger = dictionary?["danger"] as? [String:Any],
+            let settings = danger["settings"] as? [String:Any],
+            let cliArgsDictionary = settings["cliArgs"] as? [String:Any] {
             return CliArgs(dictionary: cliArgsDictionary)
         }
         
