91b6083954d6c72c4bfdfcbfff19ab43d6e742e4
Franco Meloni <franco.meloni91@gmail.com>
2018-11-30 22:10:34 +0000

    Skip komondor on release

--- Scripts/commit_new_version.sh
+++ Scripts/commit_new_version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git add Makefile CHANGELOG.md Sources/Runner/main.swift Documentation Package.swift
-git commit -m "Version $NEW_VERSION"
+git commit -m "Version $NEW_VERSION" --no-verify
 git tag "$NEW_VERSION"
-git push origin master --tags
+git push origin master --tags --no-verify
