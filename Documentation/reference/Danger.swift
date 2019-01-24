b8070d8cf864fd924752c76f21668b5bee2c2723
Franco Meloni <franco.meloni91@gmail.com>
2019-01-24 21:09:04 +0000

    Don't use codable if is not needed

--- Documentation/reference/Danger.swift
+++ Documentation/reference/Danger.swift
@@ -828,7 +828,7 @@ public struct GitHubUser: Decodable, Equatable {
 }
 
 /// Meta information for showing in the text info
-public struct Meta: Codable {}
+public struct Meta: Encodable {}
 
 public enum SpawnError: Error {
     case commandFailed(exitCode: Int32, stdout: String, stderr: String, task: Process)
@@ -845,7 +845,7 @@ public struct SwiftLint {
     public static func lint(inline: Bool = default, directory: String? = default, configFile: String? = default, lintAllFiles: Bool = default, swiftlintPath: String? = default) -> [Danger.SwiftLintViolation]
 }
 
-public struct SwiftLintViolation: Codable {
+public struct SwiftLintViolation: Decodable {
     /// Creates a new instance by decoding from the given decoder.
     ///
     /// This initializer throws an error if reading from the decoder fails, or
@@ -858,7 +858,7 @@ public struct SwiftLintViolation: Codable {
 }
 
 /// The result of a warn, message, or fail.
-public struct Violation: Codable {}
+public struct Violation: Encodable {}
 
 /// Adds an inline fail message to the Danger report
 public func fail(message: String, file: String, line: Int)
