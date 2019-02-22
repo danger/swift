0bbefb9554e71181f0e16843c7445883ea0801ea
Franco Meloni <franco.meloni91@gmail.com>
2019-02-22 22:33:21 +0000

    Support custom danger-js path

--- Sources/RunnerLib/getDangerJSPath.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import Logger
-import ShellOut
-
-public func getDangerCommandPath(logger: Logger) throws -> String {
-    logger.debug("Finding out where the danger executable is")
-    return try shellOut(to: "which danger").trimmingCharacters(in: .whitespaces)
-}
