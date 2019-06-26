70a281829c61c3f4705c7e35f6d05fc6d84876b7
Joseph Duffy <commits@josephduffy.co.uk>
2019-06-26 20:56:03 +0100

    Format GitHubBot JSON

--- Tests/DangerTests/GitHubTestResources/GitHubBot.swift
+++ Tests/DangerTests/GitHubTestResources/GitHubBot.swift
@@ -1,23 +1,23 @@
 
 public let GitHubBotJSON = """
 {
-"login": "dependabot-preview[bot]",
-"id": 27856297,
-"node_id": "MDM6Qm90Mjc4NTYyOTc=",
-"avatar_url": "https://avatars3.githubusercontent.com/in/2141?v=4",
-"gravatar_id": "",
-"url": "https://api.github.com/users/dependabot-preview%5Bbot%5D",
-"html_url": "https://github.com/apps/dependabot-preview",
-"followers_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/followers",
-"following_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/following{/other_user}",
-"gists_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/gists{/gist_id}",
-"starred_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/starred{/owner}{/repo}",
-"subscriptions_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/subscriptions",
-"organizations_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/orgs",
-"repos_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/repos",
-"events_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/events{/privacy}",
-"received_events_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/received_events",
-"type": "Bot",
-"site_admin": false
+    "login": "dependabot-preview[bot]",
+    "id": 27856297,
+    "node_id": "MDM6Qm90Mjc4NTYyOTc=",
+    "avatar_url": "https://avatars3.githubusercontent.com/in/2141?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/dependabot-preview%5Bbot%5D",
+    "html_url": "https://github.com/apps/dependabot-preview",
+    "followers_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/followers",
+    "following_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/following{/other_user}",
+    "gists_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/subscriptions",
+    "organizations_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/orgs",
+    "repos_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/repos",
+    "events_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/dependabot-preview%5Bbot%5D/received_events",
+    "type": "Bot",
+    "site_admin": false
 }
 """
