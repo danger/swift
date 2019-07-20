9de37db815a309927fa9eefce02402d0826f3180
Franco Meloni <franco.meloni91@gmail.com>
2019-07-20 22:17:38 +0100

    Improve BitBucketCloud model

--- Tests/DangerTests/BitBucketCloudTests.swift
+++ Tests/DangerTests/BitBucketCloudTests.swift
@@ -11,8 +11,8 @@ final class BitBucketCloudTests: XCTestCase {
         let activities = bitbucketCould.activities
 
         XCTAssertEqual(activities, [
-            BitBucketCloudActivity(comment: nil),
-            BitBucketCloudActivity(comment: nil),
+            BitBucketCloud.Activity(comment: nil),
+            BitBucketCloud.Activity(comment: nil),
         ])
     }
 
@@ -20,17 +20,17 @@ final class BitBucketCloudTests: XCTestCase {
         let comments = bitbucketCould.comments
 
         XCTAssertEqual(comments, [
-            BitBucketCloudComment(
-                content: BitBucketCloudContent(html: "<p>Test Comment</p>",
-                                               markup: "markdown",
-                                               raw: "Test Comment"),
+            BitBucketCloud.Comment(
+                content: BitBucketCloud.Content(html: "<p>Test Comment</p>",
+                                                markup: "markdown",
+                                                raw: "Test Comment"),
                 createdOn: Date(timeIntervalSince1970: 1_563_141_023.276),
                 deleted: false,
                 id: 109_197_958,
                 inline: nil,
                 type: "pullrequest_comment",
                 updatedOn: Date(timeIntervalSince1970: 1_563_141_023.284),
-                user: BitBucketCloudUser(
+                user: BitBucketCloud.User(
                     accountId: "557058:cac1aa4b-db5e-4c3a-8ba3-c4880b498de8",
                     displayName: "Franco Meloni",
                     nickname: "Zagolo",
@@ -42,16 +42,16 @@ final class BitBucketCloudTests: XCTestCase {
 
     func testParsesCommits() {
         let commits = bitbucketCould.commits
-        let expectedAuthor = BitBucketCloudCommit.Author(raw: "Franco Meloni <franco.meloni91@gmail.com>",
-                                                         user: BitBucketCloudUser(
-                                                             accountId: "557058:cac1aa4b-db5e-4c3a-8ba3-c4880b498de8",
-                                                             displayName: "Franco Meloni",
-                                                             nickname: "f-meloni",
-                                                             uuid: "{bd1991e4-a3ed-45b2-be38-acea659650f1}"
-                                                         ))
+        let expectedAuthor = BitBucketCloud.Commit.Author(raw: "Franco Meloni <franco.meloni91@gmail.com>",
+                                                          user: BitBucketCloud.User(
+                                                              accountId: "557058:cac1aa4b-db5e-4c3a-8ba3-c4880b498de8",
+                                                              displayName: "Franco Meloni",
+                                                              nickname: "f-meloni",
+                                                              uuid: "{bd1991e4-a3ed-45b2-be38-acea659650f1}"
+                                                          ))
 
         XCTAssertEqual(commits, [
-            BitBucketCloudCommit(
+            BitBucketCloud.Commit(
                 author: expectedAuthor,
                 date: Date(timeIntervalSince1970: 1_563_114_728.0),
                 hash: "9ab48765728b309d88486a52bb57805fffe20410",
@@ -73,7 +73,7 @@ final class BitBucketCloudTests: XCTestCase {
         let destination = pr.destination
 
         XCTAssertEqual(pr.author,
-                       BitBucketCloudUser(
+                       BitBucketCloud.User(
                            accountId: "557058:cac1aa4b-db5e-4c3a-8ba3-c4880b498de8",
                            displayName: "Franco Meloni",
                            nickname: "f-meloni",
@@ -83,7 +83,7 @@ final class BitBucketCloudTests: XCTestCase {
         XCTAssertEqual(pr.description, "README.md edited online with Bitbucket")
         XCTAssertEqual(destination.branchName, "master")
         XCTAssertEqual(destination.commitHash, "0f3153d8ba78")
-        XCTAssertEqual(destination.repository, BitBucketCloudRepo(
+        XCTAssertEqual(destination.repository, BitBucketCloud.Repo(
             fullName: "f-meloni/danger-kotlin",
             name: "danger-kotlin",
             uuid: "{37c7bd5b-887c-4450-99cb-3cab9c456f3d}"
@@ -93,7 +93,7 @@ final class BitBucketCloudTests: XCTestCase {
         XCTAssertEqual(pr.reviewers, [])
         XCTAssertEqual(source.branchName, "Franco-Meloni/readmemd-edited-online-with-bitbucket-1563114726802")
         XCTAssertEqual(source.commitHash, "9ab48765728b")
-        XCTAssertEqual(source.repository, BitBucketCloudRepo(
+        XCTAssertEqual(source.repository, BitBucketCloud.Repo(
             fullName: "f-meloni/danger-kotlin",
             name: "danger-kotlin",
             uuid: "{37c7bd5b-887c-4450-99cb-3cab9c456f3d}"
