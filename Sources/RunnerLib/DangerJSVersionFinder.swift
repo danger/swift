5742f366c75a881a615b3811e89c4ac81c64ad4d
Franco Meloni <franco.meloni91@gmail.com>
2019-11-24 18:56:19 +0100

    Add error message when danger js is not installed

--- Sources/RunnerLib/DangerJSVersionFinder.swift
+++ Sources/RunnerLib/DangerJSVersionFinder.swift
@@ -2,11 +2,11 @@ import DangerShellExecutor
 import Logger
 
 public final class DangerJSVersionFinder {
-    public static func findDangerJSVersion(dangerJSPath: String) throws -> String {
-        return try findDangerJSVersion(dangerJSPath: dangerJSPath, executor: ShellExecutor())
+    public static func findDangerJSVersion(dangerJSPath: String) -> String {
+        return findDangerJSVersion(dangerJSPath: dangerJSPath, executor: ShellExecutor())
     }
 
-    static func findDangerJSVersion(dangerJSPath: String, executor: ShellExecuting) throws -> String {
+    static func findDangerJSVersion(dangerJSPath: String, executor: ShellExecuting) -> String {
         Logger().debug("Finding danger-js version")
 
         return executor.execute(dangerJSPath, arguments: ["--version"])
