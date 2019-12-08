df830e4bb1f4501ae087b241df70820947d3ff85
Franco Meloni <franco.meloni91@gmail.com>
2019-12-08 22:50:34 +0000

    Manually point RequestKit 2.4.0

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
         .package(url: "https://github.com/shibapm/Logger", from: "0.1.0"),
         .package(url: "https://github.com/JohnSundell/Marathon", from: "3.3.0"),
         .package(url: "https://github.com/nerdishbynature/octokit.swift", .branch("master")),
+        .package(url: "https://github.com/nerdishbynature/RequestKit", from: "2.4.0"),
         // Danger Plugins
         // Dev dependencies
         .package(url: "https://github.com/shibapm/Komondor", from: "1.0.0"), // dev
