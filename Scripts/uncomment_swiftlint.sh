0dab99a0a0524551af3097b6c8eec589f279afd4
Franco Meloni <franco.meloni91@gmail.com>
2020-02-18 20:57:53 +0000

    Add script to release a version of dockerfile with swiftlint

---
+++ Scripts/uncomment_swiftlint.sh
@@ -0,0 +1,11 @@
+sed -E "s/# (.*) # swiftlint/\1 # swiftlint/" Dockerfile > Dockerfile2 && mv Dockerfile2 Dockerfile
+
+git add Dockerfile
+git commit -m "Release Dockerfile with swiftlint for version $VERSION" --no-verify
+git tag "$VERSION-swiftlint"
+
+sed -E "s/(.*) # swiftlint/# \1 # swiftlint/" Dockerfile > Dockerfile2 && mv Dockerfile2 Dockerfile
+
+git add Dockerfile
+
+
