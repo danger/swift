7659e6d69ba634049536a4c452a228232712f26a
Franco Meloni <franco.meloni91@gmail.com>
2021-10-04 02:12:54 +0100

    Unhide dependencies

--- Package.swift
+++ Package.swift
@@ -18,12 +18,12 @@ let package = Package(
         .package(name: "OctoKit", url: "https://github.com/nerdishbynature/octokit.swift", from: "0.11.0"),
         // Danger Plugins
         // Dev dependencies
-//        .package(url: "https://github.com/shibapm/Komondor", from: "1.0.0"), // dev
-//        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
-//        .package(url: "https://github.com/Realm/SwiftLint", from: "0.38.0"), // dev
-//        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.7.1"), // dev
-//        .package(url: "https://github.com/shibapm/Rocket", from: "0.4.0"), // dev
-//        .package(url: "https://github.com/SwiftDocOrg/swift-doc", .branch("1.0.0-rc.1")), // dev
+        .package(url: "https://github.com/shibapm/Komondor", from: "1.0.0"), // dev
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
+        .package(url: "https://github.com/Realm/SwiftLint", from: "0.38.0"), // dev
+        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.7.1"), // dev
+        .package(url: "https://github.com/shibapm/Rocket", from: "0.4.0"), // dev
+        .package(url: "https://github.com/SwiftDocOrg/swift-doc", .branch("1.0.0-rc.1")), // dev
     ],
     targets: [
         .target(name: "Danger-Swift", dependencies: ["Danger"]),
@@ -33,9 +33,9 @@ let package = Package(
         .target(name: "RunnerLib", dependencies: ["Logger", "DangerShellExecutor"]),
         .target(name: "Runner", dependencies: ["RunnerLib", "Logger", "DangerDependenciesResolver"]),
         .target(name: "DangerFixtures", dependencies: ["Danger"]),
-//        .testTarget(name: "DangerTests", dependencies: ["Danger", "DangerFixtures", "SnapshotTesting"]),  // dev
-//        .testTarget(name: "RunnerLibTests", dependencies: ["RunnerLib", "SnapshotTesting"], exclude: ["__Snapshots__"]), // dev
-//        .testTarget(name: "DangerDependenciesResolverTests", dependencies: ["DangerDependenciesResolver", "SnapshotTesting"], exclude: ["__Snapshots__"]),  // dev
+        .testTarget(name: "DangerTests", dependencies: ["Danger", "DangerFixtures", "SnapshotTesting"]),  // dev
+        .testTarget(name: "RunnerLibTests", dependencies: ["RunnerLib", "SnapshotTesting"], exclude: ["__Snapshots__"]), // dev
+        .testTarget(name: "DangerDependenciesResolverTests", dependencies: ["DangerDependenciesResolver", "SnapshotTesting"], exclude: ["__Snapshots__"]),  // dev
     ]
 )
 
