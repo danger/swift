f11e2595b0e02ae3792991699ffe6529befff6ec
Antoine van der Lee <ajvanderlee@gmail.com>
2021-10-13 10:35:01 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,13 @@
 // swift-tools-version:5.3
-
 import PackageDescription
 
 // Version number can be found in Source/Danger/Danger.swift
-
 let package = Package(
     name: "danger-swift",
     products: [
         .library(name: "Danger", type: .dynamic, targets: ["Danger"]),
         .library(name: "DangerFixtures", type: .dynamic, targets: ["DangerFixtures"]),
-        .library(name: "DangerDeps", type: .dynamic, targets: ["Danger-Swift"]),
+//        .library(name: "DangerDeps", type: .dynamic, targets: ["Danger-Swift"]), // dev
         .executable(name: "danger-swift", targets: ["Runner"]),
     ],
     dependencies: [
@@ -18,12 +16,12 @@ let package = Package(
         .package(name: "OctoKit", url: "https://github.com/nerdishbynature/octokit.swift", from: "0.11.0"),
         // Danger Plugins
         // Dev dependencies
-        .package(url: "https://github.com/shibapm/Komondor", from: "1.0.0"), // dev
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
-        .package(url: "https://github.com/Realm/SwiftLint", from: "0.38.0"), // dev
-        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.7.1"), // dev
-        .package(url: "https://github.com/shibapm/Rocket", from: "0.4.0"), // dev
-        .package(url: "https://github.com/SwiftDocOrg/swift-doc", .branch("1.0.0-rc.1")), // dev
+//        .package(url: "https://github.com/shibapm/Komondor", from: "1.0.0"), // dev
+//        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
+//        .package(url: "https://github.com/Realm/SwiftLint", from: "0.38.0"), // dev
+//        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.7.1"), // dev
+//        .package(url: "https://github.com/shibapm/Rocket", from: "0.4.0"), // dev
+//        .package(url: "https://github.com/SwiftDocOrg/swift-doc", .branch("1.0.0-rc.1")), // dev
     ],
     targets: [
         .target(name: "Danger-Swift", dependencies: ["Danger"]),
@@ -33,9 +31,9 @@ let package = Package(
         .target(name: "RunnerLib", dependencies: ["Logger", "DangerShellExecutor"]),
         .target(name: "Runner", dependencies: ["RunnerLib", "Logger", "DangerDependenciesResolver"]),
         .target(name: "DangerFixtures", dependencies: ["Danger"]),
-        .testTarget(name: "DangerTests", dependencies: ["Danger", "DangerFixtures", "SnapshotTesting"]),  // dev
-        .testTarget(name: "RunnerLibTests", dependencies: ["RunnerLib", "SnapshotTesting"], exclude: ["__Snapshots__"]), // dev
-        .testTarget(name: "DangerDependenciesResolverTests", dependencies: ["DangerDependenciesResolver", "SnapshotTesting"], exclude: ["__Snapshots__"]),  // dev
+//        .testTarget(name: "DangerTests", dependencies: ["Danger", "DangerFixtures", "SnapshotTesting"]),  // dev
+//        .testTarget(name: "RunnerLibTests", dependencies: ["RunnerLib", "SnapshotTesting"], exclude: ["__Snapshots__"]), // dev
+//        .testTarget(name: "DangerDependenciesResolverTests", dependencies: ["DangerDependenciesResolver", "SnapshotTesting"], exclude: ["__Snapshots__"]),  // dev
     ]
 )
 
