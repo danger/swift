5ab66af49a2f2f8bd6316253cbd104ce74db95d9
Franco Meloni <franco.meloni91@gmail.com>
2021-11-14 17:45:23 +0000

    Add dynamic only on install

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import PackageDescription
 let package = Package(
     name: "danger-swift",
     products: [
-        .library(name: "Danger", type: .dynamic, targets: ["Danger"]),
+        .library(name: "Danger", targets: ["Danger"]),
         .library(name: "DangerFixtures", targets: ["DangerFixtures"]),
         .library(name: "DangerDeps", type: .dynamic, targets: ["Danger-Swift"]), // dev
         .executable(name: "danger-swift", targets: ["Runner"]),
