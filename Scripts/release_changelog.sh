6bdd4a274b98f8b6adac8556bb4c4b1630ea2824
Franco Meloni <franco.meloni91@gmail.com>
2021-11-27 15:23:42 +0000

    Remove echo

--- Scripts/release_changelog.sh
+++ Scripts/release_changelog.sh
@@ -2,6 +2,4 @@ TEXT=`cat CHANGELOG.md| sed -n "/##\ $VERSION/,/##/p"`
 
 TEXT=`echo "$TEXT" | sed '1d;$d'`
 
-echo "gh release create $VERSION -n \"## What's Changed\r\n$TEXT\""
-
 gh release create $VERSION -n "$TEXT"
