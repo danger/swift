104285160a27d7143e3947209be1c2a80ad09c27
Franco Meloni <franco.meloni91@gmail.com>
2021-11-27 15:22:55 +0000

    Fix release creation script

--- Scripts/release_changelog.sh
+++ Scripts/release_changelog.sh
@@ -2,4 +2,6 @@ TEXT=`cat CHANGELOG.md| sed -n "/##\ $VERSION/,/##/p"`
 
 TEXT=`echo "$TEXT" | sed '1d;$d'`
 
-gh release create $VERSION -m "$TEXT"
+echo "gh release create $VERSION -n \"## What's Changed\r\n$TEXT\""
+
+gh release create $VERSION -n "$TEXT"
