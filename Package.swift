a14c233b622b3b9b2661a3995cc1e3668360f5e0
417-72KI <417.72ki@gmail.com>
2022-06-10 01:58:47 +0900

    apply `SE-0294` https://github.com/apple/swift-evolution/blob/main/proposals/0294-package-executable-targets.md

--- Package.swift
+++ Package.swift
@@ -45,7 +45,7 @@ let package = Package(
         .target(name: "DangerDependenciesResolver", dependencies: ["DangerShellExecutor", "Version", "Logger"]),
         .target(name: "Danger", dependencies: ["OctoKit", "Logger", "DangerShellExecutor"]),
         .target(name: "RunnerLib", dependencies: ["Logger", "DangerShellExecutor", "Version"]),
-        .target(name: "Runner", dependencies: ["RunnerLib", "Logger", "DangerDependenciesResolver"]),
+        .executableTarget(name: "Runner", dependencies: ["RunnerLib", "Logger", "DangerDependenciesResolver"]),
         .target(name: "DangerFixtures", dependencies: ["Danger"]),
     ] + devTargets
 )
