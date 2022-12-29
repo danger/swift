87916ae128cfdc73db48580c3af067a56d5a35da
Hiroma <hiroma.tsuchiya@rakuten.com>
2022-12-29 18:50:45 +0900

    Fix a lint error

--- Sources/RunnerLib/NSRegularExpression+FilesImport.swift
+++ Sources/RunnerLib/NSRegularExpression+FilesImport.swift
@@ -3,5 +3,5 @@ import Foundation
 extension NSRegularExpression {
     private static let fileImportPattern = "\\/\\/[\\ ]?fileImport:\\ (.*)"
 
-    static let filesImportRegex = try! NSRegularExpression(pattern: fileImportPattern, options: .allowCommentsAndWhitespace)
+    static let filesImportRegex = try! NSRegularExpression(pattern: fileImportPattern, options: .allowCommentsAndWhitespace) // swiftlint:disable:this force_try
 }
