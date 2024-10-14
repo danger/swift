f3a717ef0dcca780ee779e28ee3c3b577fa6af50
Franco Meloni <franco.meloni91@gmail.com>
2024-10-14 14:48:58 +0200

    More fixed lint changes that would not work on older swift version

--- Sources/RunnerLib/DangerSwiftOption.swift
+++ Sources/RunnerLib/DangerSwiftOption.swift
@@ -6,7 +6,7 @@ public enum DangerSwiftOption: String, CaseIterable {
     public var hasParameter: Bool {
         switch self {
         case .dangerJSPath:
-            true
+            return true
         }
     }
 }
@@ -17,7 +17,7 @@ public enum DangeSwiftRunnerOption: String, CaseIterable {
     public var hasParameter: Bool {
         switch self {
         case .cwd:
-            true
+            return true
         }
     }
 }
